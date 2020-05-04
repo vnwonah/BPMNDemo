00fea9aa7d93bcb060751dcd6f2e3cc9550a9b5e
Vincent Nwonah <vnwonah@outlook.com>
2020-05-04 21:41:23 +0100

    Added engine test

---
+++ EngineTest/Program.cs
@@ -0,0 +1,46 @@
+﻿using Engine.Interfaces;
+using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EngineTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // copy flow.bpmn to executing assembly dir
+
+            var p = new Process(File.OpenRead("flow.bpmn"));
+            var processInstance = p.NewProcessInstance();
+            processInstance.SetDefaultHandlers();
+            processInstance.SetHandler("task", new MyTaskHandler());
+            processInstance.SetHandler("startEvent", new MyStartHandler());
+
+            var processVar = new Dictionary<string, object>() { { "processVar1", "value" }, { "processVar2", 50 } };
+            processInstance.Start(processVar);
+            Console.ReadLine();
+        }
+
+        private class MyStartHandler : INodeHandler
+        {
+            public void Execute(ProcessNode currentNode, ProcessNode previousNode)
+            {
+                Console.WriteLine("Custom Start Handler");
+                Console.WriteLine(currentNode.NodeName);
+                currentNode.Done();
+            }
+        }
+
+        private class MyTaskHandler : INodeHandler
+        {
+            public void Execute(ProcessNode currentNode, ProcessNode previousNode)
+            {
+                Console.WriteLine("Custom Task Handler");
+                Console.WriteLine(currentNode.NodeName);
+                currentNode.Done();
+            }
+        }
+    }
+}
